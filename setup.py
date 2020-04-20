941c1a42864019c63624dc8bfbfbfefc3fc5af09
Laura Damaceno de Almeida <41930302+lauraDamacenoAlmeida@users.noreply.github.com>
2020-04-20 09:39:31 -0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="hello",
+    version="0.0.1",
+    description="Meu primeiro pacote",
+    author="Laura Damaceno de Almeida",
+    packages=["Hello"]
+)
